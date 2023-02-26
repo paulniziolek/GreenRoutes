1f6995b30a03ac83e9eeff56711a39b850dc86d8
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-26 04:34:26 -0500

    Added google maps API
    Co-authored by Paul Niziolek niziolek.paul@gmail.com
    Co-authored by Nicholas Chen nickchen12345@gmail.com

--- frontend/src/components/MapsAPI.js
+++ frontend/src/components/MapsAPI.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 
 const MapsAPI = () => {
 
+    let data = {
+            "origin":{
+                "location":{
+                    "latLng":{
+                        "latitude": 37.419734,
+                        "longitude": -122.0827784
+                        }
+                }
+            },
+            "destination":{
+                "location":{
+                    "latLng":{
+                        "latitude": 37.417670,
+                        "longitude": -122.079595
+                        }
+                }
+            },
+            "routeModifiers": {
+                "vehicleInfo": {
+                    "emissionType": "GASOLINE"
+                }
+            },
+            "travelMode": "DRIVE",
+            "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
+            "requestedReferenceRoutes": ["FUEL_EFFICIENT"],
+            "departureTime": "2023-10-15T15:01:23.045123456Z",
+            "computeAlternativeRoutes": false,
+            "routeModifiers": {
+            "avoidTolls": false,
+            "avoidHighways": false,
+            "avoidFerries": false
+            },
+            "languageCode": "en-US",
+            "units": "IMPERIAL",
+        }
+        //'FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline,routes.distanceMeters,routes.duration,routes.routeLabels,routes.travelAdvisory.fuelConsumptionMicroliters,routes.routeToken',
+    useEffect(() => {
+        axios.post("https://routes.googleapis.com/directions/v2:computeRoutes?key=AIzaSyBn9s9PGTebqGkhGYCDQzdm7s3YOBirUKM&fields=routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline", data)
+        .then(
+            response => {
+                console.log("SUCCESS", response)
+
+            })
+        .catch(error => console.log(error.response))
+    })
 
 
     return (
