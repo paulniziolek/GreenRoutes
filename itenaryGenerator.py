84a3130874d5d97b6cbd554e01dc53c4b1e7bec0
yummybomb <nickchen12345@gmail.com>
2023-02-25 22:55:12 -0500

    ItenaryGenerator 98% :)

---
+++ itenaryGenerator.py
@@ -0,0 +1,18 @@
+import os
+import openai
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def iGenerator(startLoc, endLoc):
+# Load your API key from an environment variable or secret management service
+    
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    prompt = "In an ecological manner doing outdoor activities, make me an activities guide within a reaosnable amount of time in the way of an itenary on my way from" + startLoc +  " to " + endLoc + "\n For example: \n Time = HH:MM \n decisions = a location someone might want to stop at AM/PM examples: \n time: decisions"
+    response = openai.Completion.create(model="text-davinci-003", prompt= prompt, temperature=0.7, max_tokens=1200)
+    responseText = response.choices[0].text
+    
+    return responseText
+print(iGenerator("5 Thurston Dr, Livingston, NJ 07039","1978 River Road, Bushkill, PA 18324"))
+    
